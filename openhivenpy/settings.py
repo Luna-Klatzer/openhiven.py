30408416479577bdbe4d99bf3d447c69751a76fc
Nicolas-Klatzer <nicolas.klatzer@gmail.com>
2021-01-11 15:44:22 +0100

    Fixed setuptools build error

--- openhivenpy/settings.py
+++ openhivenpy/settings.py
@@ -1,5 +1,5 @@
 def load_env():
     from dotenv import load_dotenv, find_dotenv
 
-    # Loading the .env data
-    load_dotenv(find_dotenv())
+    # Loading the openhivenpy.env data
+    load_dotenv(find_dotenv("openhivenpy.env"))
