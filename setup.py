69c81e89fee0de974f1da63d7354887c6b64a1c0
Nicolas-Klatzer <nicolas.klatzer@gmail.com>
2021-02-10 21:05:38 +0100

    Updated version to 0.1.3.dev2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as file:
 
 setuptools.setup(
     name="openhivenpy",
-    version="0.1.3.dev1",
+    version="0.1.3.dev2",
     author="Nicolas Klatzer",
     author_email="nicolas.klatzer@gmail.com",
     description="The OpenSource Python API Wrapper for Hiven!",
