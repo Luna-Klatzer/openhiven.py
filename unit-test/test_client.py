69131ba868684caaf699247b223a1ad3c3c117e5
Nicolas-Klatzer <nicolas.klatzer@gmail.com>
2021-02-16 14:19:36 +0100

    Fixed client closing

--- unit-test/test_client.py
+++ unit-test/test_client.py
@@ -5,3 +5,9 @@ def test_user_client(token):
     """ Testing specifically targeted at the UserClient Class """
     user = openhivenpy.UserClient(token)
     assert isinstance(user, openhivenpy.UserClient)
+
+    @user.event()
+    async def on_ready():
+        await user.close()
+
+    user.run()
